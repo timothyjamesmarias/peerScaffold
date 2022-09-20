8f75d3840d1bc8523e26a80f55a6d58de4c02cfd
Timothy Marias <timothyjamesmarias@gmail.com>
2022-09-20 15:31:10 -0700

    created layout files and crud files for models

---
+++ resources/js/Components/Nav.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import { Inertia } from '@inertiajs/inertia';
+    import NavLink from './NavLink.vue';
+    import ApplicationLogo from './ApplicationLogo.vue';
+
+    const logout = () => {
+        Inertia.post(route('logout'));
+    };
+
+</script>
+
+<template>
+    
+    <nav class="flex justify-between p-4 bg-zinc-900 text-stone-200 sticky top-0 z-50">
+            <Link href="/">
+                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+            </Link>
+                
+        <ul v-if="$page.props.user" class="flex">
+            <li class="pl-4"><NavLink :href="route('dashboard')">Account</NavLink></li>
+            <li class="pl-4">
+                <form method="POST" @submit.prevent="logout">
+                    <NavLink as="button">Log Out</NavLink>
+                </form>
+            </li>
+        </ul>
+
+        <ul v-else class="flex">
+            <li><NavLink :href="route('login')">Sign In</NavLink></li>
+            <li class="pl-4"><NavLink :href="route('register')">Sign Up</NavLink></li>
+        </ul>
+
+    </nav>
+
+</template>
+
+<style scoped>
+
+    
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,15 +6,25 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Link, Head } from '@inertiajs/inertia-vue3';
+import DefaultLayout from '../js/Layouts/AppLayout.vue';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, app, props, plugin }) {
+    resolve: (name) => {
+        const page = resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'));
+        page.then((module) =>{
+            module.default.layout = module.default.layout || DefaultLayout;
+        });
+        return page;
+    },
+        setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .component("Link", Link)
+            .component("Head", Head)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
